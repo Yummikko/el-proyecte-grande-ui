0ae39cb84da0f0a9351a0acf22051e9a148e56a9
Duc Trung Bui <txbuiduc@gmail.com>
2023-03-29 23:16:44 +0200

    new front end for Add Dream

---
+++ dream-catcher-ui/src/components/FileUploader.js
@@ -0,0 +1,23 @@
+import React, {useRef} from 'react'
+
+const FileUploader = ({onFileSelectError, onFileSelectSuccess}) => {
+    const fileInput = useRef(null)
+
+
+    const handleFileInput = (e) => {
+        // handle validations
+        const file = e.target.files[0];
+        if (file.size > 1024)
+          onFileSelectError({ error: "File size cannot exceed more than 1MB" });
+        else onFileSelectSuccess(file);
+      };
+
+    return (
+        <div className="mb-3">
+            <label for="formFile" className="form-label">Add Image</label>
+            <input className="form-control" type="file" id="formFile" onChange={handleFileInput} />
+        </div>
+    )
+}
+
+export default FileUploader
