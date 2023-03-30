c16a391c092781938d53349bdbf79d0e95365d3f
Yummikko <ola.chyla0@gmail.com>
2023-03-30 22:37:35 +0200

    added changes in OfferDetails.js, added offer-details.css

--- dream-catcher-ui/src/components/pages/OfferDetails.js
+++ dream-catcher-ui/src/components/pages/OfferDetails.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import ImageService from "../../services/ImageService";
-import "../../styles/global.css"
+import "../../styles/offer-details.css"
+import defaultPhoto from '../../assets/images/Default.jpeg';
+import Navbar from "../Navbar";
+
 
 function OfferDetails() {
   const { id } = useParams();
@@ -19,25 +22,44 @@ function OfferDetails() {
   }
 
   return(
+    <div>
+     <Navbar/><br/>
     <div className="h-100 gradient-custom-2">
         <div className="container py-5 h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
+            <div className="row">
+            <h1 className="title">OFFER DETAILS</h1>
                 <div className="col col-lg-9 col-xl-7">
                     <div className="card">
                         <div className="rounded-top text-white d-flex flex-row">
                             <div className="ms-4 mt-5 d-flex flex-column text-dark align-items-center">
-                                <ImageService data={offer}/>
+                            {offer.image ? (
+                            <ImageService data={offer} className="dream-image" />
+                            ) : (
+                            <img
+                                src={defaultPhoto}
+                                alt="dream"
+                                className="dream-image"
+                            />
+                            )}
                                 <h1 className="mv-2" >{offer.title}</h1>
-                                <p>{offer.type}</p>
+                                <p>Type: {offer.type}</p>
                                 <p>{offer.description}</p>
                             </div>
                         </div>
                         <div className="p-4 text-black">
                             <div className="d-flex justify-content-center text-center py-1">
-                                <div>
-                                    {/* <p className="mb-1 h5">{dream.likes}</p> */}
-                                    <p className="small text-muted mb-0">Likes</p>
-                                </div>
+                                     <div>
+                                        {/* <p className="mb-1 h5">{dream.likes}</p> */}
+                                        <p className="small text-muted mb-0">Likes</p>
+                                    </div>
+                                    <div className="px-3">
+                                        {/* { dream.comments && <p className="mb-1 h5">{Object.keys(dream.comments).length}</p> } */}
+                                        <p className="small text-muted mb-0">Comments</p>
+                                    </div>
+                                    <div>
+                                        {/* <p className="mb-1 h5">{dream.views}</p> */}
+                                        <p className="small text-muted mb-0">Views</p>
+                                    </div>
                          
                             </div>
                         </div>
@@ -46,11 +68,12 @@ function OfferDetails() {
                                 <p className="lead fw-normal mb-0">Price: {offer.price}$</p>
                             </div>
                         </div>
-                        <button>BUY NOW!</button>
+                        <button className="Button">BUY NOW!</button>
                     </div>
                 </div>
             </div>
         </div>
+    </div>
     </div>
 )
 
