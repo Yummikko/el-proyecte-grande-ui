69e6f5bd76f37b3eb7ac59e8dc214fdb94b997a2
Pastor28 <dgorz@interia.com>
2023-04-28 11:54:44 +0200

    BoardAdmin

--- dream-catcher-ui/src/App.js
+++ dream-catcher-ui/src/App.js
@@ -24,6 +24,7 @@ import OfferDetails from "./components/pages/OfferDetails";
 import OfferPage from "./components/pages/OfferPage";
 import AddOffer from "./components/pages/AddOffer";
 import MentorProfile from './components/pages/MentorProfile';
+import AdminProfile from './components/pages/AdminProfile';
 
 class App extends Component {
 
@@ -52,6 +53,7 @@ class App extends Component {
             <Route path="/add-offer/:id" element={<AddOffer />} />
             <Route path="/:nickname" element={<PublicProfile />} />
             <Route path="/mentor/:nickname" element={<MentorProfile />} />
+            <Route path="/admins" element={<AdminProfile />} />
 
           </Routes>
         </div>
