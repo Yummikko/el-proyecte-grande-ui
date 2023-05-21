1ca2d59e2202715d29fa53c19f4ed6fa708a9559
Trung Bui Duc <txbuiduc@gmail.com>
2023-05-21 11:50:02 +0200

    new changes

--- dream-catcher-ui/src/components/pages/AddDream.js
+++ dream-catcher-ui/src/components/pages/AddDream.js
@@ -56,8 +56,8 @@ const AddDream = () => {
             DreamService.uploadFile(selectedFile)
         })
         .then(async() => {
-            // await delay(5000)
-            // refreshPage()
+            await delay(5000)
+            refreshPage()
         })
     }
 
@@ -76,38 +76,38 @@ const AddDream = () => {
     <Navbar/>
         <div className='flex max-w-2xl mx-auto mt-5 shadow border-b pb-2'>
         
-        {<div className='p-2'>
-            <div className='font-thin text-2xl tracking-wider mt-3 mb-5'>
-                <h1 className='header'>Add dream</h1>
+            {<div className='p-2'>
+                <div className='font-thin text-2xl tracking-wider mt-3 mb-5'>
+                    <h1 className='header'>Add dream</h1>
+                </div>
+                <div className='items-center justify-center h-14 w-full my-4'>
+                    <label className='d-block'>Dream Title</label>
+                    <input type="text" className='dream-title-text h-10 border mt-2 px-2 py-2' name="dreamTitle" value={dream.dreamTitle} onChange={(e) => handleChange(e)} required />
+                </div>
+                <div className='items-center justify-center h-14 w-full my-4'>
+                    <label className='d-block'>Dream Description</label>
+                    <textarea type="text" className='dream-desc-text h-10 border mt-2 px-2 py-2' name="dreamDescription" value={dream.dreamDescription} onChange={(e) => handleChange(e)} required />
+                </div>
+                <TagsInput setTagsData={(tags) => handleTagsChange(tags)} />
+                <FileUploader onFileSelectSuccess={(file) => handleFileChange(file)}
+            onFileSelectError={({ error }) => alert(error)} />
+                <div className='items-center justify-center h-14 w-full my-4'>
+                    <button onClick={saveDream} className='btn btn-primary mb-3'>Add</button>
+                    <button className='btn btn-info mb-3 ms-1'>Clear</button>
+                </div>
+            </div>}
+            {data && (
+            <div className='form-group m-1'>
+                <div
+                className={data ? 'alert alert-success' : 'alert alert-danger'}
+                role='alert'
+                >
+                {message}
+                </div>
             </div>
-            <div className='items-center justify-center h-14 w-full my-4'>
-                <label className='d-block'>Dream Title</label>
-                <input type="text" className='dream-title-text h-10 border mt-2 px-2 py-2' name="dreamTitle" value={dream.dreamTitle} onChange={(e) => handleChange(e)} required />
+            )
+            }
             </div>
-            <div className='items-center justify-center h-14 w-full my-4'>
-                <label className='d-block'>Dream Description</label>
-                <textarea type="text" className='dream-desc-text h-10 border mt-2 px-2 py-2' name="dreamDescription" value={dream.dreamDescription} onChange={(e) => handleChange(e)} required />
-            </div>
-            <TagsInput setTagsData={(tags) => handleTagsChange(tags)} />
-            <FileUploader onFileSelectSuccess={(file) => handleFileChange(file)}
-          onFileSelectError={({ error }) => alert(error)} />
-            <div className='items-center justify-center h-14 w-full my-4'>
-                <button onClick={saveDream} className='btn btn-primary mb-3'>Add</button>
-                <button className='btn btn-info mb-3 ms-1'>Clear</button>
-            </div>
-        </div>}
-        {data && (
-        <div className='form-group m-1'>
-            <div
-            className={data ? 'alert alert-success' : 'alert alert-danger'}
-            role='alert'
-            >
-            {message}
-            </div>
-        </div>
-        )
-        }
-        </div>
         </div>
     )
 }
